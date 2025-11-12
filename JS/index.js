03fb205b7a9425589e25fa8fc231139a883add9f
Rodrick Heffley <codemoneky25@outlook.com>
2025-11-11 20:55:24 -0500

    add import intiCardSearch JS

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,9 @@
 import { initThemeToggle } from "./JS lib/Global/light-dark-mode.js";
+import { initCardSearch } from './JS lib/Global/search.js';
+
+
+
+//--------------------
+initCardSearch(); // uses default selectors
 
 initThemeToggle(); // by default looks for #theme-toggle
