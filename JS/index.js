198cebd8f2c74eebe0a9a0b491969b32868381da
Rodrick Heffley <codemoneky25@outlook.com>
2025-11-13 16:57:54 -0500

    add import link for header-module

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,11 @@
 import { initThemeToggle } from "./JS lib/Global/light-dark-mode.js";
 import { initCardSearch } from './JS lib/Global/search.js';
-
+import {  loadHeader  } from "./JS lib/Global/Modules/header-module.js";
 
 
 //--------------------
 initCardSearch(); // uses default selectors
 
 initThemeToggle(); // by default looks for #theme-toggle
+
+loadHeader(); // header module
